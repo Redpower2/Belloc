f44c8f52739f764342ab48bd46eceaafd1599c90
Redpower2 <steam.mauro36@gmail.com>
2026-08-21 11:01:23 -0300

    por las dudas

--- src/commands/offrol/decir/logica.ts
+++ src/commands/offrol/decir/logica.ts
@@ -4,7 +4,7 @@ import { numeroRandom } from "../../../utils/general/numeroRandom";
 const defolt = ["Quiero tetas.", "¿Qué significa el bro respeta?", "https://images-ext-1.discordapp.net/external/tILJSgZwXWhAkf9YFqesd7o0tXSEaXmBEHgp7qsXILA/https/www.unbosque.edu.co/sites/default/files/2025-04/Facultad.gif", "Cállate.", "Os follaré con la polla"];
 
 //me da miedo que me reporten el bot o que raideen con él, yo dejaria hacer quilombo eh
-const filtro = ["https", "@", "nigger", "nazi", "hitler", "nigga", "fag", "foid", "cp", "infantil", "judío", "judio"]
+const filtro = ["http", "@", "nigger", "nazi", "hitler", "nigga", "fag", "foid", "cp", "infantil", "judío", "judio"]
 
 
 export async function execute(interaction: ChatInputCommandInteraction)
