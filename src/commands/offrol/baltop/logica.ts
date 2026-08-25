9f31bfa2a9a42a8f52ba9a5c30daaa03339eb1d8
Redpower2 <steam.mauro36@gmail.com>
2026-08-25 15:06:57 -0300

    baltop

--- src/commands/offrol/baltop/logica.ts
+++ src/commands/offrol/baltop/logica.ts
@@ -1,6 +1,51 @@
 import { ChatInputCommandInteraction } from "discord.js";
+import { Usuarios } from "../../../schemas/usuario";
+import { colores } from "../../../utils/general/colores";
 
 
 export async function execute(interaction: ChatInputCommandInteraction)
 {
+    const usuarios = await Usuarios.find({});
+    if(usuarios.length === 0)
+    {
+        return await interaction.reply({
+            content: "Son todos pobres en el servidor."
+        });
+    }
+    const users = await Promise.all(
+        usuarios.map(u =>
+        {
+            return {
+                nombre: u.nombre,
+                dinero: u.dinero + u.banco
+            }
+        })
+    );
+
+    const top = users
+        .sort((a, b) =>
+        {
+            return b.dinero - a.dinero;
+        })
+        .slice(0, 10);
+
+    const emojis = ["💎", "💰", "💲"];
+    const descripcion = top
+    .map(({ nombre, dinero }, i) =>
+    {
+        const emoji = emojis[i] ?? "🪙";
+        return `${emoji} ${i + 1}. ${nombre}: ${dinero}`;
+    }
+    )
+    .join("\n");
+
+    return await interaction.reply({
+        embeds: [
+            {
+                title: "Los más ricos",
+                description: descripcion,
+                color: colores.personaje
+            }
+        ]
+    })
 }
