782bb971e01b8805f4fa455dbb1ad852df26d348
Redpower2 <steam.mauro36@gmail.com>
2026-09-10 15:12:15 -0300

    lesto

--- src/commands/offrol/manager/sub8.ts
+++ src/commands/offrol/manager/sub8.ts
@@ -1,4 +1,4 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, ComponentType, ContainerBuilder, MessageFlags, SectionBuilder, SeparatorBuilder, SeparatorSpacingSize, TextDisplayBuilder } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, ContainerBuilder, MessageFlags, SectionBuilder, SeparatorBuilder, SeparatorSpacingSize, TextDisplayBuilder } from "discord.js";
 import { MINUTO } from "../../../utils/general/tiempo";
 import { modalAñadir, modalEdit } from "./modal";
 import { esNumero } from "../../../utils/general/esNumero";
@@ -7,122 +7,150 @@ import { TiendaItems } from "../../../schemas/tiendaItem";
 import { Items } from "../../../schemas/item";
 import { responder } from "../../../utils/general/responder";
 
-export async function itemsTienda(interaction: ChatInputCommandInteraction)
+async function armarTienda()
 {
-    const tienda = await TiendaItems.find({});
-//Paginado
-    function armarContainer(itemsTienda: TiendaItem[])
+    let tienda = await TiendaItems.find({});
+    const tiendasArray: TiendaItem[][] = [];
+    let index = -1;
+    for(let i = 0 ; i < tienda.length ; i++)
     {
-        const container = new ContainerBuilder()
+        if(i%16 === 0)
+        {
+            index += 1;
+            tiendasArray.push([]);
+        }
+        tiendasArray[index].push(tienda[i]);
+    }
+    return tiendasArray;
+}
+
+async function armarMensaje(interaction: ChatInputCommandInteraction, tiendas: TiendaItem[][], indice: number)
+{
+    let tienda = tiendas[indice];
+    const container = new ContainerBuilder()
+        .addTextDisplayComponents(
+            new TextDisplayBuilder()
+                .setContent(`# Tienda del servidor`)
+        )
+        .addSeparatorComponents(
+            new SeparatorBuilder()
+                .setDivider(true)
+                .setSpacing(SeparatorSpacingSize.Small)
+        );
+    if(!tiendas.length)
+    {
+        container
             .addTextDisplayComponents(
                 new TextDisplayBuilder()
-                    .setContent(`# Tienda del servidor`)
+                    .setContent("## Sin items")
             )
-            .addSeparatorComponents(
-                new SeparatorBuilder()
-                    .setDivider(true)
-                    .setSpacing(SeparatorSpacingSize.Small)
-            )
-        
-        if(!itemsTienda.length)
-        {
-            container
-                .addTextDisplayComponents(
-                    new TextDisplayBuilder()
-                        .setContent("## Sin items")
-                )
-                
-        }
-        else
-        {
-            itemsTienda.forEach(
-                function(i)
-                {
-                    const itemText = [`### [${i.relacion}] ${i.nombre.toLowerCase()}${i.emoji} — $${i.precio}`];
-                    if(i.descuento)
-                    {
-                        itemText.push(` (-${i.descuento})`)
-                    }
-                    container
-                        .addSectionComponents(
-                            new SectionBuilder()
-                                .addTextDisplayComponents(
-                                    new TextDisplayBuilder()
-                                        .setContent(itemText.join(""))
-                                )
-                                .setButtonAccessory(
-                                    new ButtonBuilder()
-                                        .setCustomId(`editar-${i.nombre}`)
-                                        .setStyle(ButtonStyle.Secondary)
-                                        .setEmoji("🛒")
-                                        .setLabel("Editar")
-                                )
-                        )
-                }
-            )
-        }
-        return container;
     }
-    let container = armarContainer(tienda);
-    const row = new ActionRowBuilder<ButtonBuilder>()
-        .setComponents(
-            new ButtonBuilder()
-                .setCustomId("añadiritem")
-                .setStyle(ButtonStyle.Primary)
-                .setEmoji("🏷️")
-                .setLabel("Añadir Item")
+    else
+    {
+        tienda.forEach(
+            function(i)
+            {
+                const itemText = [`### [${i.relacion}] ${i.nombre}${i.emoji} — $${i.precio}`];
+                if(i.descuento)
+                {
+                    itemText.push(` (-${i.descuento})`)
+                }
+                container
+                    .addSectionComponents(
+                        new SectionBuilder()
+                            .addTextDisplayComponents(
+                                new TextDisplayBuilder()
+                                    .setContent(itemText.join(""))
+                            )
+                            .setButtonAccessory(
+                                new ButtonBuilder()
+                                    .setCustomId(`editar-${i.id}`)
+                                    .setStyle(ButtonStyle.Primary)
+                                    .setEmoji("🛒")
+                                    .setLabel("Editar")
+                            )
+                    )
+            }
         )
-        
-    const mensaje = await responder(interaction, {
-        components: [container, row],
+    }
+    const row = new ActionRowBuilder<ButtonBuilder>()
+    const añadirBoton = new ButtonBuilder()
+        .setCustomId("añadiritem")
+        .setStyle(ButtonStyle.Secondary)
+        .setEmoji("🏷️");
+    if(tiendas.length > 1)
+    {
+        row
+            .setComponents(
+                new ButtonBuilder()
+                    .setCustomId("paginaminus")
+                    .setStyle(ButtonStyle.Primary)
+                    .setEmoji("⬅️"),
+                añadirBoton,
+                new ButtonBuilder()
+                    .setCustomId("paginaplus")
+                    .setStyle(ButtonStyle.Primary)
+                    .setEmoji("➡️")
+            );
+    }
+    else
+    {
+        añadirBoton
+            .setLabel("Añadir Item")
+        row
+            .setComponents(
+                añadirBoton
+            );
+    }
+    return await responder(interaction, {
+        components: [
+            container, row
+        ],
         flags: MessageFlags.IsComponentsV2
     });
-    const collector = mensaje.createMessageComponentCollector({
-        componentType: ComponentType.Button,
-        filter: i => i.user.id === interaction.user.id,
-        time: 8 * MINUTO
-    });
-    collector.on("collect",
-        async function(b)
-        {
-            await b.showModal(
-                b.customId === "añadiritem"
-                    ? modalAñadir
-                    : modalEdit
-            );
-            const modalEnviado = await b.awaitModalSubmit({
-                filter: i => i.user.id === interaction.user.id,
-                time: 5 * MINUTO
-            })
-            .catch(() => null);
-            if(!modalEnviado)
-            {
-                return console.log("!modalEnviado");
-            }
-            if(!modalEnviado.isFromMessage())
-            {
-                return;
-            }
-            const campos = modalEnviado.fields;
-            const emoji = campos.getTextInputValue("añadiremoji");
-            const precio = campos.getTextInputValue("añadirprecio");
-            const precioNum = Number(precio)
-            if(!esEmoji(emoji) || !esNumero(precio) || precioNum <= 0)
-            {
-                return await responder(modalEnviado, {
-                    content: "Emoji o precio inválido",
-                    flags: MessageFlags.Ephemeral
-                });
-            }
+}
 
-            if(b.customId === "añadiritem")
-            {
-                const nombre = campos.getTextInputValue("añadirnombre");
-                const relacion = (campos.getStringSelectValues("añadirrelacion")[0] as  "comprable" | "vendible");
+export async function itemsTienda(interaction: ChatInputCommandInteraction)
+{
+    let indice = 0;
+    let tiendas = await armarTienda();
+    const mensaje = await armarMensaje(interaction, tiendas, indice);
+    const collector = mensaje.createMessageComponentCollector({
+        filter: i => i.user.id === interaction.user.id,
+        time: 10 * MINUTO
+    });
+    collector.on("collect", async b => 
+    {
+        let tienda = tiendas[indice];
+        switch(b.customId)
+        {
+            case "añadiritem":
+                await b.showModal(modalAñadir);
+                const modalEnv = await b.awaitModalSubmit({
+                    filter: i => i.user.id === interaction.user.id,
+                    time: 5 * MINUTO
+                }).catch(() => null);
+                if(!modalEnv)
+                {
+                    return;
+                }
+                const { fields } = modalEnv;
+                const emoji = fields.getTextInputValue("añadiremoji");
+                const precio = fields.getTextInputValue("añadirprecio");
+                const precioNum = Number(precio)
+                if(!esEmoji(emoji) || !esNumero(precio) || precioNum <= 0)
+                {
+                    return await responder(modalEnv, {
+                        content: "Emoji o precio inválido",
+                        flags: MessageFlags.Ephemeral
+                    });
+                }
+                const nombre = fields.getTextInputValue("añadirnombre");
+                const relacion = (fields.getStringSelectValues("añadirrelacion")[0] as  "comprable" | "vendible");
                 const item = await Items.findOne({ nombre: nombre });
                 if(!item)
                 {
-                    return await responder(modalEnviado, {
+                    return await responder(modalEnv, {
                         content: "Ese item no existe!",
                         flags: MessageFlags.Ephemeral
                     });
@@ -135,80 +163,125 @@ export async function itemsTienda(interaction: ChatInputCommandInteraction)
                     relacion: relacion
                 };
                 await TiendaItems.create(nuevoItem);
-                container = armarContainer(tienda);
-                await modalEnviado.update({
-                    components: [container, row]
-                });
-            }
-            else if(b.customId.startsWith("editar"))
-            {
-                const itemNombre = b.customId.replace("editar-", "");
-                const itemTienda = tienda.find(i => i.nombre === itemNombre);
+                modalEnv.deferUpdate();
+            break;
+            case "paginaminus":
+                indice -= 1
+                if(indice < 0)
+                {
+                    indice = tienda.length - 1
+                }
+            break;
+            case "paginaplus":
+                indice += 1
+                if(indice > tienda.length - 1)
+                {
+                    indice = 0;
+                }
+            break;
+            default:
+                //editar
+                await b.showModal(modalEdit);
+                const modalEnv2 = await b.awaitModalSubmit({
+                    filter: i => i.user.id === interaction.user.id,
+                    time: 5 * MINUTO
+                }).catch(() => null);
+                if(!modalEnv2)
+                {
+                    return;
+                }
+                const fields2 = modalEnv2.fields;
+                const emoji2 = fields2.getTextInputValue("añadiremoji");
+                if(emoji2 && !esEmoji(emoji2))
+                {
+                    return await responder(modalEnv2, {
+                        content: "Emoji inválido",
+                        flags: MessageFlags.Ephemeral
+                    })
+                }
+                const precio2 = fields2.getTextInputValue("añadirprecio");
+                const precioNum2 = Number(precio2)
+                if(precio2 && (!esNumero(precio2) || precioNum2 <= 0))
+                {
+                    return await responder(modalEnv2, {
+                        content: "Precio inválido",
+                        flags: MessageFlags.Ephemeral
+                    })
+                }
+                const itemId = Number(b.customId.replace("editar-", ""));
+                const itemTienda = tienda.find(i => i.id === itemId);
                 if(!itemTienda)
                 {
                     return console.error("Por alguna razón, ese item no existe en la tienda.")
                 }
 
-                const retirado = campos.getCheckbox("retiraritem");
+                const retirado = fields2.getCheckbox("retiraritem");
                 if(retirado)
                 {
                     await TiendaItems.deleteOne({
                         nombre: itemTienda.nombre
                     });
-                    container = armarContainer(tienda);
-                    return await modalEnviado.update({
-                        components: [container, row]
-                    });
                 }
-
-                const descuento = campos.getTextInputValue("añadirdescuento");
-                if(descuento && itemTienda.relacion === "vendible")
+                else
                 {
-                    return await responder(modalEnviado, {
-                        content: "¡No se puede poner descuento a un item vendible!",
-                        flags: MessageFlags.Ephemeral
-                    });
-                }
-                const descuentoNum = Number(descuento);
-                if(descuentoNum >= 100 || descuentoNum <= 0)
-                {
-                    return await responder(modalEnviado, {
-                        content: "El descuento tiene que ser entre 1 y 99.",
-                        flags: MessageFlags.Ephemeral
-                    });
-                }
-
-                tienda.forEach(
-                    function(i)
+                    const descuento = fields2.getTextInputValue("añadirdescuento");
+                    const descuentoNum = Number(descuento);
+                    if(descuento)
                     {
-                        if(i.nombre === itemTienda.nombre)
+                        if(descuentoNum >= 100 || descuentoNum <= 0)
                         {
-                            i.emoji = emoji;
-                            i.precio = precioNum;
-                            i.descuento = descuentoNum;
+                            return await responder(modalEnv2, {
+                                content: "El descuento tiene que ser entre 1 y 99.",
+                                flags: MessageFlags.Ephemeral
+                            });
                         }
                     }
-                )
-                container = armarContainer(tienda);
-                await modalEnviado.update({
-                    components: [container, row]
-                });
-            }
+                    if(itemTienda.relacion === "vendible")
+                    {
+                        return await responder(modalEnv2, {
+                            content: "¡No se puede poner descuento a un item vendible!",
+                            flags: MessageFlags.Ephemeral
+                        });
+                    }
+
+                    const itemEditado: TiendaItemOptions = {};
+                    if(emoji2)
+                    {
+                        itemEditado.emoji = emoji2;
+                    }
+                    if(precio2)
+                    {
+                        itemEditado.precio = precioNum2;
+                    }
+                    if(descuento)
+                    {
+                        itemEditado.descuento = descuentoNum
+                    }
+
+                    await TiendaItems.updateOne(
+                        {
+                            id: itemId
+                        },
+                        itemEditado
+                    );
+                }
+                modalEnv2.deferUpdate();
+            break;
         }
-    );
-    collector.on("end",
-        async function(_, reason)
+        tiendas = await armarTienda();
+        await armarMensaje(interaction, tiendas, indice);
+    });
+    collector.on("end", async (_, reason) => 
+    {
+        switch(reason)
         {
-            switch(reason)
-            {
-                case "time":
-                    const texto = new TextDisplayBuilder()
-                        .setContent("Comando finalizado.")
-                    return await responder(interaction, {
-                        components: [texto],
-                        flags: MessageFlags.IsComponentsV2
-                    });
-            }
+            case "time":
+                const texto = new TextDisplayBuilder()
+                    .setContent("Comando finalizado.")
+                return await responder(interaction, {
+                    components: [texto],
+                    flags: MessageFlags.IsComponentsV2
+                });
         }
-    )
+    })
 }
--- src/types/TiendaItem.d.ts
+++ src/types/TiendaItem.d.ts
@@ -7,3 +7,13 @@ interface TiendaItem
     descuento?: number;
     relacion: "comprable" | "vendible";
 }
+
+interface TiendaItemOptions
+{
+    id?: number;
+    nombre?: string;
+    emoji?: string;
+    precio?: number;
+    descuento?: number;
+    relacion?: "comprable" | "vendible";
+}
